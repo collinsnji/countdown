18201e83827cd08b3b5868ea7bf7ae23bab1c844
Collin Grimm <collins.geek@gmail.com>
2016-12-31 18:51:45 +0100

    Creating timer controller

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+setInterval(() => {
+    var date = new Date();
+    var time = {
+      hour   : date.getHours(),
+      minute : date.getMinutes(),
+      second : date.getSeconds()
+    };
+    var current_time;
+    var offset = {
+      offsethHour = 24,
+      offsetMinute = 60
+     };
+    var clock_wrapper = document.getElementById('clock-wrapper');
+
+    if (time.hour < 10) time.hour = "0"+time.hour;
+    if (time.minute < 10) time.minute = "0"+time.minute;
+    if (time.second < 10) time.second = "0"+time.second;
+     current_time = (offset.offsetHour - time.hour) + ":" 
+                  + (offset.offsetMinute - time.minute) + ":" 
+                  + (offset.offsetMinute - time.second);
+
+    clock_wrapper.innerHTML = current_time;
+ }, 500);
