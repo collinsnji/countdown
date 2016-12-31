5fae8f3d0ef11ce811707e1537951c2bb815cc35
Collin Grimm <collins.geek@gmail.com>
2016-12-31 19:10:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ setInterval(() => {
      };
     var clock_wrapper = document.getElementById('clock-wrapper');
 
-    if (time.hour < 10) time.hour = "0"+time.hour;
     if (time.minute < 10) time.minute = "0"+time.minute;
     if (time.second < 10) time.second = "0"+time.second;
      current_time = (offset.offsetHour - time.hour) + ":" 
